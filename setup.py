075a3d7e52a11201aac6ab738095ead4d6935664
Grahame Bowland <grahame@angrygoats.net>
2017-08-31 11:03:15 +0800

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ckanext-datesearch',
-    version='0.3.0',
+    version='0.3.0ccg1',
     description='CKAN extension for publication year facet',
     long_description=
     '''
